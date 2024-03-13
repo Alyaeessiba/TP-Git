b20450e4122088f5984fa669f43d24a5acb49e6f
Alyaeessiba <alyaeessiba0@gmail.com>
2024-03-13 10:03:24 +0000

    up

--- test.java
+++ test.java
@@ -1,10 +1,7 @@
 class Test{
-<<<<<<< HEAD
 	system.out("hi");    
-=======
 	syeeeazeaeazeaoeeaz
 		eazeaeazzae
 ezaeazaezazeaze
 		eazeazeazzzstem    
->>>>>>> 8f4d9687cdd4fb9e9524e69218922dcb0e17d14e
 }
